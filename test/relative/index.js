24efecc6b8f600c313c638acc50522d266659fad
Eric Lindvall <eric@5stops.com>
2011-08-22 12:11:55 -0700

    Ensure parsing 'now' works.

--- test/relative/index.js
+++ test/relative/index.js
@@ -28,6 +28,10 @@
     'tom = "Tomorrow"': {
       run: function() { },
       assert: function() { return Date.today().add(1).day().equals(Date.parse('tom')) }
+    },
+    'now': {
+      run: function() { this.date = Date.parse('now') },
+      assert: function() { return new Date().set({millisecond: 0}).equals(this.date.set({millisecond: 0})) }
     }
   },
 'Relative Days': {
